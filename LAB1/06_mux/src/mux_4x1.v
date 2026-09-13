dae49fba40a24bc7d6326f073516505b8a0574ed
김윤지 <yunuos2525@gmail.com>
2026-09-14 03:59:35 +0900

    LAB1 mux intentional selection failure

--- LAB1/06_mux/src/mux_4x1.v
+++ LAB1/06_mux/src/mux_4x1.v
@@ -1,4 +1,4 @@
 module mux_4x1(input wire [3:0] i, input wire [1:0] s, output wire z);
     // Preserve the original lab ordering: 00 -> i[3].
-    assign z = i[3-s];
+    assign z = i[s];
 endmodule
