9ce18d8b4b2f54a3a8b1066d09f01771a89fb65f
김윤지 <yunuos2525@gmail.com>
2026-09-13 23:33:03 +0900

    LAB1 full adder intentional OR to AND failure

--- LAB1/02_full_adder/src/full_adder.v
+++ LAB1/02_full_adder/src/full_adder.v
@@ -5,6 +5,6 @@ module full_adder(input wire a, b, cin, output wire s, cout);
     half_adder first(a, b, partial_sum, carry_ab);
     half_adder second(partial_sum, cin, s, carry_cin);
 
-    assign cout = carry_ab | carry_cin;
+    assign cout = carry_ab & carry_cin;
 
 endmodule
