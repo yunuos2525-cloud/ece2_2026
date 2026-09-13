f8d09485edd4f8a710f6e6d40c92bfc9af503475
김윤지 <yunuos2525@gmail.com>
2026-09-14 01:24:41 +0900

    LAB1 4bit adder intentional plus to minus failure

--- LAB1/03_4bit_adder/src/adder_4bit.v
+++ LAB1/03_4bit_adder/src/adder_4bit.v
@@ -1,3 +1,3 @@
 module adder_4bit(input wire [3:0] a, b, output wire [3:0] s, output wire cout);
-    assign {cout, s} = {1'b0, a} + {1'b0, b};
+    assign {cout, s} = {1'b0, a} - {1'b0, b};
 endmodule
