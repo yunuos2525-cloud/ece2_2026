07e4a2942da6d60860bab4a2eb3743632695d79e
김윤지 <yunuos2525@gmail.com>
2026-09-14 04:55:38 +0900

    LAB1 demux restore right shift

--- LAB1/07_demux/src/demux_1x8.v
+++ LAB1/07_demux/src/demux_1x8.v
@@ -1,4 +1,4 @@
 module demux_1x8(input wire i, input wire [2:0] s, output wire [7:0] o);
     // Preserve the original lab ordering: 000 -> o[7].
-    assign o = i ? (8'b10000000 << s) : 8'b0;
+    assign o = i ? (8'b10000000 >> s) : 8'b0;
 endmodule
